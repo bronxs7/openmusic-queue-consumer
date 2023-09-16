9fc0ff2bc6958ca86301e363e3ef86a69e246dc5
bronxs7 <arisman7@gmail.com>
2023-09-17 05:45:40 +0700

    Fixing enviroment variable mail name

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,10 +4,10 @@ const config = {
     port: process.env.PORT,
   },
   mail: {
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    user: process.env.SMTP_USER,
-    password: process.env.SMTP_PASSWORD,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    user: process.env.MAIL_ADDRESS,
+    password: process.env.MAIL_PASSWORD,
   },
   rabbitMq: {
     server: process.env.RABBITMQ_SERVER,
